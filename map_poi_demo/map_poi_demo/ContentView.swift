983be5e65e09af467894750e997a1a6cc872baa2
Johnny Zhou <johnny.zhou@covermore.com>
2024-02-19 23:21:23 +1100

    map poi demo

---
+++ map_poi_demo/map_poi_demo/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  map_poi_demo
+//
+//  Created by Johnny1 on 18/2/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        SearchableMap()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ map_poi_demo/map_poi_demo/map_poi_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  map_poi_demoApp.swift
+//  map_poi_demo
+//
+//  Created by Johnny1 on 18/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct map_poi_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

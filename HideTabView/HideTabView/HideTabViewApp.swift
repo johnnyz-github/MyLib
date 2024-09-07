e9c5aed9e2a6ced4b3ef7e00fc55f60b48153316
Johnny Zhou <johnny.zhou@covermore.com>
2024-09-07 19:12:02 +1000

    add my memorize app

---
+++ HideTabView/HideTabView/HideTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  HideTabViewApp.swift
+//  HideTabView
+//
+//  Created by Johnny Zhou on 9/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct HideTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TabViewHideDemo/TabViewHideDemo/ChildView.swift
@@ -0,0 +1,18 @@
+//
+//  ChildView.swift
+//  TabViewHideDemo
+//
+//  Created by Johnny Zhou on 2/7/2024.
+//
+
+import SwiftUI
+
+struct ChildView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChildView()
+}
